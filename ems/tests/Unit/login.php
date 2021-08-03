5e02fe345bf4efb8748e6bc194f7dc24fb84d722
Jithu <jithu.george98@gmail.com>
2021-08-03 21:29:42 +0530

    login changes

---
+++ ems/tests/Unit/login.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class login extends TestCase
+{   
+    public function addLoginProvider()
+    {
+        return array(
+            array(1,123,'manager-records'),
+            
+        );
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
